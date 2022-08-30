925eb52351cc2920b2e50d5533d369bdde6aec99
James Gill <james.gill@elev.ga.ntig.se>
2022-08-30 10:26:37 +0200

    initial commit

---
+++ TE21A_Lucktext/Program.cs
@@ -0,0 +1,20 @@
+﻿string name;
+string land;
+string djur;
+
+Console.WriteLine ("vad heter du");
+name = Console.ReadLine();
+
+Console.WriteLine ("vilket är ditt favoritland");
+land = Console.ReadLine();
+
+Console.WriteLine ("vad är ditt favoritdjur");
+djur = Console.ReadLine();
+
+
+Console.WriteLine($"hej {name}");
+Console.WriteLine($"du bor i {land}");
+Console.WriteLine($"med en {djur}");
+
+
+Console.ReadLine();
